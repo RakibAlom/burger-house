e2f3350c394e7723550b98660ca9a4342803bc5d
Md. Rakibul Alom <47636586+RakibAlom@users.noreply.github.com>
2020-06-23 13:03:51 +0600

    first commit
    I design it with html, css, bootstrap4, javascript and owl-carousel.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+// header owl-carousel
+$('.owl-carousel').owlCarousel ({
+	loop:true,
+	autoplay:true,
+	margin:10,
+	autoplayTimeOut:4000,
+	smartSpeed:2000,
+	responsive:{
+		0:{
+			items:1
+		}
+	}
+})
